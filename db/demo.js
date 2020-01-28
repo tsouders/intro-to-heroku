b125767cb95e5ade040a7b1087f30d41995029f9
tsouders <39637014+tsouders@users.noreply.github.com>
2020-01-28 11:36:43 -0800

    Revert db

--- db/demo.js
+++ db/demo.js
@@ -1,14 +1,14 @@
 module.exports = function(client) {
 
-  client.query(`CREATE TABLE Broker__c (
+  client.query(`CREATE TABLE broker__c (
    id SERIAL NOT NULL,
    sfid CHARACTER VARYING(18) UNIQUE,
-   Name CHARACTER VARYING(80),
-   Email__c CHARACTER VARYING(80),
-   Phone__c CHARACTER VARYING(40),
-   Mobile_Phone__c CHARACTER VARYING(40),
-   Title__c CHARACTER VARYING(30),
-   Picture__c CHARACTER VARYING(255)
+   name CHARACTER VARYING(80),
+   email__c CHARACTER VARYING(80),
+   phone__c CHARACTER VARYING(40),
+   mobile_phone__c CHARACTER VARYING(40),
+   title__c CHARACTER VARYING(30),
+   picture__c CHARACTER VARYING(255)
   );`);
 
   client.query(`CREATE TABLE property__c (
@@ -19,7 +19,7 @@ module.exports = function(client) {
    beds__c DOUBLE PRECISION,
    address__c CHARACTER VARYING(100),
    baths__c DOUBLE PRECISION,
-   broker__c CHARACTER VARYING(18) REFERENCES Broker__c(sfid),
+   broker__c CHARACTER VARYING(18) REFERENCES broker__c(sfid),
    description__c CHARACTER VARYING(500),
    state__c CHARACTER VARYING(20),
    city__c CHARACTER VARYING(50),
@@ -38,14 +38,14 @@ module.exports = function(client) {
   );`);
 
 
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('caroline@ionicrealty.com', '617-244-3672', 'Caroline Kingsley', '617-244-3672', 'a0036000003SsJwAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/caroline_kingsley.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('jen@ionicrealty.com', '617-244-3672', 'Jennifer Wu', '617-244-3672', 'a0036000003SsJzAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('michael@ionicrealty.com', '617-244-3672', 'Michael Jones', '617-244-3672', 'a0036000003SsJxAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('michelle@ionicrealty.com', '617-244-3672', 'Michelle Lambert', '617-244-3672', 'a0036000003SsK2AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michelle_lambert.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('jonathan@ionicrealty.com', '617-244-3672', 'Jonathan Bradley', '617-244-3672', 'a0036000003SsJyAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jonathan_bradley.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('olivia@ionicrealty.com', '617-244-3672', 'Olivia Green', '617-244-3672', 'a0036000003SsK0AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/olivia_green.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('miriam@ionicrealty.com', '617-244-3672', 'Miriam Aupont', '617-244-3672', 'a0036000003SsK1AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/miriam_aupont.jpg');`);
-  client.query(`INSERT INTO Broker__c (Email__c, Phone__c, Name, Mobile_Phone__c, sfid, Title__c, Picture__c) VALUES ('victor@ionicrealty.com', '617-244-3672', 'Victor Ochoa', '617-244-3672', 'a0036000003SsK3AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/victor_ochoa.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('caroline@ionicrealty.com', '617-244-3672', 'Caroline Kingsley', '617-244-3672', 'a0036000003SsJwAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/caroline_kingsley.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('jen@ionicrealty.com', '617-244-3672', 'Jennifer Wu', '617-244-3672', 'a0036000003SsJzAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('michael@ionicrealty.com', '617-244-3672', 'Michael Jones', '617-244-3672', 'a0036000003SsJxAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('michelle@ionicrealty.com', '617-244-3672', 'Michelle Lambert', '617-244-3672', 'a0036000003SsK2AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michelle_lambert.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('jonathan@ionicrealty.com', '617-244-3672', 'Jonathan Bradley', '617-244-3672', 'a0036000003SsJyAAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jonathan_bradley.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('olivia@ionicrealty.com', '617-244-3672', 'Olivia Green', '617-244-3672', 'a0036000003SsK0AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/olivia_green.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('miriam@ionicrealty.com', '617-244-3672', 'Miriam Aupont', '617-244-3672', 'a0036000003SsK1AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/miriam_aupont.jpg');`);
+  client.query(`INSERT INTO broker__c (email__c, phone__c, name, mobile_phone__c, sfid, title__c, picture__c) VALUES ('victor@ionicrealty.com', '617-244-3672', 'Victor Ochoa', '617-244-3672', 'a0036000003SsK3AAK', 'Senior Broker', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/victor_ochoa.jpg');`);
 
   client.query(`INSERT INTO property__c (name, thumbnail__c, beds__c, location__longitude__s, address__c, baths__c, broker__c, description__c, sfid, state__c, city__c, zip__c, title__c, picture__c, price__c, location__latitude__s) VALUES ('18 Henry st', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/realty/house01sq.jpg', 4, -71.1109500000000025, '18 Henry st', 3, 'a0036000003SsJwAAK', 'Lorem ipsum dolor sit amet', 'a0236000002NHKoAAO', 'MA', 'Cambridge', '01742', 'Stunning Victorian', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/realty/house01.jpg', 975000, 42.3566300000000027);`);
   client.query(`INSERT INTO property__c (name, thumbnail__c, beds__c, location__longitude__s, address__c, baths__c, broker__c, description__c, sfid, state__c, city__c, zip__c, title__c, picture__c, price__c, location__latitude__s) VALUES ('145 Commonwealth ave', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/realty/house12sq.jpg', 4, 71.0753109999999992, '145 Commonwealth ave', 3, 'a0036000003SsJxAAK', 'Lorem ipsum dolor sit amet', 'a0236000002NHKtAAO', 'MA', 'Boston', '02420', 'Contemporary Luxury', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/realty/house12.jpg', 845000, 42.3524659999999997);`);
